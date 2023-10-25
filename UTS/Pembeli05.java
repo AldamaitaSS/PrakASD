5799b7d93f982d8e9f89cb2f1a1acb55e5f95f59
AldamaitaSS <143566241+AldamaitaSS@users.noreply.github.com>
2023-10-25 10:16:48 +0700

    Add files via upload

---
+++ UTS/Pembeli05.java
@@ -0,0 +1,15 @@
+
+
+public class Pembeli05 {
+    String namaPembeli, NoHp;
+    int noAntrian;
+
+    public Pembeli05 (int a, String b, String c){
+        noAntrian=a;
+        namaPembeli=b;
+        NoHp=c;
+    }
+
+    public void Enqueue() {
+    }
+}
